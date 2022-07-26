d9ef37a8e40ab219d5978ff743499fe1999ac908
Van Huynh <vthuynh@cpp.edu>
2022-07-25 22:18:17 -0400

    add Global.d.ts file to avoid scss and images imports

---
+++ src/Globals.d.ts
@@ -0,0 +1,9 @@
+// avoid import scss files errors
+declare module "*.module.css";
+declare module "*.module.scss";
+
+// avoid import imgs files errors
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
