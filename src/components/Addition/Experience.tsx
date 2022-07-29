e5f7b1ff3a89ab59443757658fd26a97f7344a52
vanthuynh <vthuynh@cpp.edu>
2022-07-28 22:15:19 -0400

    change tailwind config file, begin implementing timeline

--- src/components/Addition/Experience.tsx
+++ src/components/Addition/Experience.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 
 export default function Experience() {
-    
     return (
-        <div className='pd-0'>
-            <ul className="pd-0">
+        <div className="container">
+            <ul className="timeline">
+
                 <div className="timeline-event">
                     <label className="timeline-event-icon"></label>
                     <div className="timeline-event-copy">
                         <p className="timeline-event-thumbnail">June, 2020 - Sept, 2020</p>
-                        <div className="margin-left: 10px;">
+                        <div className="tw-ml-10;">
                             <img className="width:60px; height:auto; float:right;" src="res/quickfits.png" alt=""/>
                             <h2 className="margin-bottom: 10px">Quickfits</h2>
                             <h3 className="margin-bottom: -10px">Software Engineering intern</h3>
                             <p>Build a platform that recommend daily outfits and match stylist with users.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="timeline-event">
+                    <label className="timeline-event-icon"></label>
+                    <div className="timeline-event-copy">
+                        <p className="timeline-event-thumbnail">March, 2020 - June, 2020</p>
+                        <div style="margin-left: 10px;">
+                            <img style="width:60px; height:auto; float:right; background-color:black;" src="res/commercebytes.png" alt="">
+                            <h2 style="margin-bottom: 10px">CommerceBytes</h2>
+                            <h3 style="margin-bottom: -10px">Software Engineering intern</h3>
+                            <p>Increase businesses' traffic with eCommerce and marketing</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="timeline-event">
+                    <label className="timeline-event-icon"></label>
+                    <div className="timeline-event-copy">
+                        <p className="timeline-event-thumbnail">March, 2020 - Current</p>
+                        <div style="margin-left: 10px;">
+                            <img style="width:60px; height:auto; float:right" src="res/vcla.png" alt="">
+                            <h2 style="margin-bottom: 10px">vcla.stat.ucla</h2>
+                            <h3 style="margin-bottom: -10px">research assistant</h3>
+                            <p>Build a deep learning compiler to accelerate computing time</p>
                         </div>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  mode: 'jit',
+  purge:[],
+  corePlugins: {
+    preflight: false
+  },
+  prefix:'tw-',
+  important: true, // set important to true
   theme: {
     extend: {},
   },
