ea3f06af560b7b0611e908f2c6b6e1a9478c7e59
vanthuynh <vthuynh@cpp.edu>
2024-09-03 00:04:51 -0700

    Updates

--- src/Globals.d.ts
+++
@@ -1,9 +0,0 @@
-// avoid import scss files errors
-declare module "*.module.css";
-declare module "*.module.scss";
-
-// avoid import imgs files errors
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.jpeg";
-declare module "*.gif";
